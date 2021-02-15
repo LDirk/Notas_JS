179db3d72597ed0f673ad52c8c374ec5349520c9
LDirk <47995676+LDirk@users.noreply.github.com>
2021-02-15 15:06:56 -0300

    Update principal (1).js

--- Calc_notas/js/principal (1).js
+++ Calc_notas/js/principal (1).js
@@ -6,58 +6,74 @@ titulo.textContent = "Cálculo das notas";
 
 
 // Calculando as notas
-var materia = document.querySelector("#primeira-materia");
 
-var tdvae1 = materia.querySelector(".info-VAE1");
-var vae1 = tdvae1.textContent;
-
-var tdvae2 = materia.querySelector(".info-VAE2");
-var vae2 = tdvae2.textContent;
+var materias = document.querySelectorAll(".Nota");
 
 
-vae1valido = true;
-vae2valido = true; 
+for (var i = 0; i< materias.length; i++){
 
-if (vae1<0 || vae1>10){
-	alert("Nota da vae1 Invalida");
-	vae1valido = false;
-	tdvae1.textContent = "Invalido";
-}
+	
+	var materia = materias[i]
+	
+	var tdvae1 = materia.querySelector(".info-VAE1");
+	var vae1 = tdvae1.textContent;
 
-if (vae2<0 || vae2 >10){
-	alert("Nota da vae2 invalida");
-	vae2valido = false; 
-	tdvae2.textContent = "Invalido";
-
-}
+	var tdvae2 = materia.querySelector(".info-VAE2");
+	var vae2 = tdvae2.textContent;
 
 
-if (vae1valido && vae2valido){
+	vae1valido = true;
+	vae2valido = true; 
 
-	var notafinal = (parseInt(vae1)+parseInt(vae2))/2;
+	if (vae1<0 || vae1>10){
 
-	var tdnotafinal = materia.querySelector(".info-NotaFinal");
+		alert("Nota da vae1 Invalida");
+		vae1valido = false;
+		tdvae1.textContent = "Invalido";
 
-	tdnotafinal.textContent = notafinal;
-
-}
-
-
-// Calculando a situação ( Aprovado ou reprovado)
-
-var tdsituacao = materia.querySelector(".info-Situação");
-
-if (vae1valido && vae2valido){
-
-	if (notafinal>=7){
-		tdsituacao.textContent = "Aprovado"
+		materia.style.color = "black";
+		materia.style.backgroundColor = "LightCoral";	
+	
 	}
 
-	else{
-		tdsituacao.textContent = "Reprovado"
+	if (vae2<0 || vae2 >10){
+		alert("Nota da vae2 invalida");
+		vae2valido = false; 
+		tdvae2.textContent = "Invalido";
+
+		materia.style.color = "black";
+		materia.style.backgroundColor = "LightCoral"
+
+
+	}
+
+
+	if (vae1valido && vae2valido){
+
+		var notafinal = (parseInt(vae1)+parseInt(vae2))/2;
+
+		var tdnotafinal = materia.querySelector(".info-NotaFinal");
+
+		tdnotafinal.textContent = notafinal;
+	}
+
+	// Calculando a situação ( Aprovado ou reprovado)
+
+	var tdsituacao = materia.querySelector(".info-Situação");
+
+	if (vae1valido && vae2valido){
+
+		if (notafinal>=7){
+			tdsituacao.textContent = "Aprovado";
+		}
+		else if (notafinal<7 && notafinal >=0){
+			tdsituacao.textContent = "Reprovado";
+		}
+		else{
+			tdsituacao.textContent = "";
+			tdnotafinal.textContent ="";
+		}
 	}
 }
-
-
 
 
